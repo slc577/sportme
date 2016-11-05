f2dcfb252b5c74dcfbac0909a19acf84f6299809
slc577 <sharlene88875@gmail.com>
2016-11-04 20:23:30 -0400

    updated bot again

--- index.js
+++ index.js
@@ -81,31 +81,40 @@ function sendGenericMessage(sender) {
     let messageData = {
         "attachment": {
             "type": "template",
-            "payload": {
+            "payload":
+            {
                 "template_type": "generic",
-                "elements": [{
+                "elements":
+                [
+                {
                     "title": "First card",
                     "subtitle": "Element #1 of an hscroll",
                     "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
+                    "buttons":
+                    [{
                         "type": "web_url",
                         "url": "https://www.messenger.com",
                         "title": "web url"
-                    }, {
+                    },
+                    {
                         "type": "postback",
                         "title": "Postback",
                         "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
+                    }
+                    ],
+                }/*,
+                
                     "title": "Second card",
                     "subtitle": "Element #2 of an hscroll",
                     "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
+                    "buttons":
+                    [{
                         "type": "postback",
                         "title": "Postback",
                         "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
+                	}],
+                }*/
+                ]
             }
         }
     }
