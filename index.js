35054afc2594ccd4a5c01b323dec85d7c74061ba
slc577 <sharlene88875@gmail.com>
2016-11-06 16:19:14 -0500

    updated bot again

--- index.js
+++ index.js
@@ -60,7 +60,8 @@ const token = "EAAWquF1fimYBAD3SZBrTEXELeUC3GY4oydKcZCzTx3X22fvbcEvksysnx8DZApmQ
 
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
+    //let messageData = { text:text }
+    let messageData = { text:'whoo'  }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
